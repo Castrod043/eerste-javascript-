b4806ff62295a74d00b07bb4bdcfc21401ddbcfb
Castrod043 <64606385+Castrod043@users.noreply.github.com>
2021-09-06 22:08:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//log mijn naam
+let name = 'David';
+console.log(name)
+
+
+// optel som 
+let y = 5;
+let x = y + 2;
+console.log(x)
+
+//vermenigvuldigen
+let a = 24;
+let c = a * 234;
+console.log(c)
+
+// nummer variable 
+let age = 48;
+console.log(typeof age)
+
+// string variable 
+let leeftijd = "48";
+console.log(typeof leeftijd)
+
